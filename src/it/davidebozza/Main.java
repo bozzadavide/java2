3493b8298d409607ff09a2715acf8250edd8fa35
Davide Bozza <bozza.davide@gmail.com>
2018-01-06 22:39:30 +0100

    primo commit

--- src/it/davidebozza/Main.java
+++ src/it/davidebozza/Main.java
@@ -4,13 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] i;
-        i = new int[10];
-        for (int j=0; j < 10; j++)
-            i[j] = j;
-        Point p = new Point();
-        p.x = 1;
-        p.y = 2;
         System.out.println("Hello, World");
     }
 }
